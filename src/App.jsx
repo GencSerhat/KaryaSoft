49ca43c8d4d9925caf51d972b13fd48fca715931
Serhat GENÇ <gncsrht05@gmail.com>
2026-02-06 12:33:21 +0300

    eimzapage frontend görünümler düzemlendi yayına hazır

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import EImzaPage from "./pages/EImzaPage/EImzaPage";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>
-       <Home/>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/e-imza" element={<EImzaPage />} />
+    </Routes>
   );
 }
 
