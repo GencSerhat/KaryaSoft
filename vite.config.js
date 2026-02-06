4c66b45ed4af16af07d320a5b3a14b1f434c32f6
Serhat GENÇ <gncsrht05@gmail.com>
2026-02-06 14:56:23 +0300

    vite değiştirilid

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Vercel için kök, GH Pages tabanı kaldırıldı
+  base: '/', // 🔴 BU ÇOK ÖNEMLİ
 })
